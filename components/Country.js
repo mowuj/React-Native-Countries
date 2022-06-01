76b94a370f6049cb4a1cc8cf3356d83cd87b78be
mowuj <mowuj92@gmail.com>
2022-06-01 22:50:59 +0600

    setup react-navite app

---
+++ components/Country.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+
+export default function Country({country}) {
+  return (
+    <View>
+          <Text>Country: {country.name.common }</Text>
+         
+    </View>
+  )
+}
